8d09e9ef99c39838cd2f2db1e70226b8d6cbc77e
Deepak Kumar Battini <deepakkumar1984@gmail.com>
2017-12-12 19:54:05 +1030

    Update installdep-gpu.sh

--- install/installdep-gpu.sh
+++ install/installdep-gpu.sh
@@ -3,9 +3,15 @@ sudo apt-get update && apt-get install -y python-setuptools python-pip python-de
 sudo apt-get install -y libopencv-dev libssl-dev libffi-dev libfann-dev swig tesseract-ocr python-tk openmpi-bin
 
 python -m pip install --upgrade pip
+git clone https://github.com/libfann/fann.git
+cd ./fann
+cmake .
+sudo make install
+cd ..
+
 sudo pip install setuptools Flask Cython Keras tensorflow mxnet-cu80 sklearn pandas matplotlib h5py pillow requests simplejson opencv-python jsonpickle pytesseract nltk chatterbot urllib3 easydict padatious seaborn mpld3 psutil nvidia-ml-py flask_cors passlib flask-sqlalchemy
 sudo pip install -e git+https://github.com/mycroftai/adapt#egg=adapt-parser
-sudo apt-get -y install openmpi-bin
+
 sudo pip install https://cntk.ai/PythonWheel/GPU/cntk-2.1-cp27-cp27mu-linux_x86_64.whl
 sudo apt-get clean
 sudo apt-get autoremove -y
